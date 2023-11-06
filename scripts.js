dd7b696a2dec935b1c77de1975bfe7ddaca6162f
Nikholas Boyd <boydnikholas96@gmail.com>
2023-11-05 22:27:36 -0500

    Added initial starter files

--- scripts.js
+++ scripts.js
@@ -1,1 +1,18 @@
-/* Add your JavaScript to this file */
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.querySelector("form");
+    const Input = document.querySelector("#email");
+    const messageDiv = document.querySelector(".message");
+
+    form.addEventListener("submit", function (event) {
+        event.preventDefault();
+
+        const email = Input.value.trim();
+
+        if (email === "") {
+            messageDiv.textContent = "Please enter a valid email address.";
+        } else {
+            messageDiv.textContent = `Thank you! Your email address ${email} has been added to our mailing list!`;
+            emailInput.value = ""; 
+        }
+    });
+});
